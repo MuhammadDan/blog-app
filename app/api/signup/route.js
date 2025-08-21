7dbe74b94e58aa7052645bc8c8830bd742fee275
MuhammadDan <daniyalk154330@gmail.com>
2025-08-21 23:27:05 +0500

    Today summary: feat(auth): implement password hashing using bcrypt in userSchema pre-save hook
    - Added secure password encryption before saving user to DB
    - Used bcrypt with 12 salt rounds for optimal security
    - Ensured middleware runs only during user creation
    - Documented logic in Roman Urdu for beginner reference

--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -5,15 +5,25 @@ import { NextResponse as res } from "next/server"
  
 export const POST = async(request)=>{
     try {
-        const body = await request.json() 
-        return res.json(body)
+        const body = await request.json()
+        const user = new UserSchema(body)
+        await user.save() 
+        return res.json(user)
+        // return res.json(body)
     } catch (err) {
-        console.log(err);
-        
+        // console.log(err);
+        return res.json(
+            {success: false, message: err.message},
+            {status: 500}
+        )
     }
 }
 
 // export const GET = async () => {
 //   return res.json({ message: "Signup API is working. Use POST method to send data." })
 // }
-// yai promise return karta hai islyai await lagaya hai
+// yai promise return karta hai islyai await lagaya hai 
+
+//   const user = new UserSchema(body) --> jo data userSchema mai save hoa hoga wo res.json ki help sai retun hojyega  
+//        await user.save() 
+ //       return res.json(user)
--- schema/user.schema.js
+++ schema/user.schema.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from 'bcrypt';
 const userSchema = new Schema({
     fullname: {
         type: String,
@@ -18,6 +18,14 @@ const userSchema = new Schema({
     }
 })
 
+userSchema.pre("save",async function(next){
+      const encrypted = await bcrypt.hash(this.password.toString(),12)
+      this.password = encrypted
+      next()
+    // console.log(this.password)
+})
 // const UserSchema = mongoose.model('User',userSchema) 
 const UserSchema = mongoose.models.User || mongoose.model('User',userSchema) 
 export default UserSchema
+
+// hum nai userschema mai pre save ordinary funcion banai gai next ko receive or agai barhai gai or console kar wa rahai hai jo bhi password hoga.
